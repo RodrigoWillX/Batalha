a584c1185a7b98f5c323e7a4d7ced477c6a7bb77
Rodrigo Will <44774584+RodrigoWillX@users.noreply.github.com>
2018-11-16 09:38:18 -0200

    Add files via upload

---
+++ ConsoleApp2/ConsoleApp2/Jogador.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp2
+{
+    class Jogador
+    {
+        //Atributos 
+        public string nome;
+        public float dinheiro;
+        public int forca, ataque, defesa, velocidade;
+
+        //Metodos
+        public void Atacar()
+        {
+            Console.WriteLine(nome + " atacou com forca " + ataque);
+        }
+
+        //Metodo defender com parametro inimigo(string)
+        public void Defender(string inimigo)
+        {
+            Console.WriteLine(nome + " defendeu o ataque de " + inimigo);
+        }
+        
+        public void Atacar(string inimigo)
+        { 
+            Console.WriteLine(nome + " atacou o ataque de " + inimigo);
+            }
+
+        //Metodo construtor
+        public Jogador(string defineNome, float defineDinheiro, int defineForca, int defineAtaque, int defineDefesa, int defineVelocidade)
+        {
+            nome = defineNome;
+            dinheiro = defineDinheiro;
+            forca = defineForca;
+            ataque = defineAtaque;
+            defesa = defineDefesa;
+            velocidade = defineVelocidade;
+            Console.WriteLine(nome + " foi criado!");
+        }
+    }
+}
---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Cria objeto Jogador
+            Jogador diego = new Jogador("Diego", 500, 300, 300, 200, 190);
+            Jogador miguel = new Jogador("Miguel", 500, 300, 300, 200, 190);
+            Jogador giovanni = new Jogador("Giovanni", 500, 300, 300, 200, 190);
+
+            float mediaForcejog = (diego.forca + miguel.forca + giovanni.forca) / 3;
+            float mediaAtaquejog = (diego.ataque + miguel.ataque + giovanni.ataque) / 3;
+            float mediaVelocidadejog = (diego.velocidade + miguel.velocidade + giovanni.velocidade) / 3;
+
+            Console.WriteLine("A media da forca dos inimigos rebeldes eh: " +  mediaForcejog);
+            Console.WriteLine("A media do ataque dos inimigos rebeldes eh: " + mediaAtaquejog);
+            Console.WriteLine("A media da velocidade dos inimigos rebeldes eh: " + mediaVelocidadejog);
+            
+            float TotalDinheiro = (diego.dinheiro + miguel.dinheiro + giovanni.dinheiro) / 3;
+            Console.WriteLine("A media do dinheiro dos inimigos rebeldes eh: " + TotalDinheiro);
+
+
+            //Cria objeto Inimigo
+            Inimigo roberta = new Inimigo("Roberta", 900, 320, 400, 300, 200);
+        Inimigo mia = new Inimigo("Mia", 900, 350, 400, 250, 210);
+        Inimigo lupita = new Inimigo("Lupita", 100, 150, 120, 160, 150);
+
+        float mediaForceini = (roberta.forca + mia.forca + lupita.forca) / 3;
+        float mediaAtaqueini = (roberta.ataque + mia.ataque + lupita.ataque) / 3;
+        float mediaVelocidadeini = (roberta.velocidade + mia.velocidade + lupita.velocidade) / 3;
+
+            Console.WriteLine("A media da forca das inimigas rebeldes eh: " + mediaForceini);
+            Console.WriteLine("A media do ataque das inimigas rebeldes eh: " + mediaAtaqueini);
+            Console.WriteLine("A media da velocidade das inimigas rebeldes eh: " + mediaVelocidadeini);
+
+            float TotalDinheiroInimigo = (roberta.dinheiro + mia.dinheiro + lupita.dinheiro) / 3;
+            Console.WriteLine("A media do dinheiro das inimigas rebeldes eh: " + TotalDinheiroInimigo);
+            Console.ReadLine();
+        }
+
+    }
+    
+}
+
+
